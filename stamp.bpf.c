f24200321fa79ba9fa058ee6584a2e72fd8fccba
Giulio Sidoretti <giuliosidoretti@gmail.com>
2022-01-14 15:04:49 +0000

    code clean: variables and includes

--- stamp.bpf.c
+++ stamp.bpf.c
@@ -4,18 +4,14 @@
 #define HIKE_PROG_NAME    stamp
 
 /*
- * works on IPv6 packets
- * assumes that a program (usually, the classifier) has
- * already parsed the program up to the network layer
- * i.e. cur->nhoff is set
- *
- * TODO : may be some errors could be handled instead of dropping
- * packet, considering that this is a debug tool
+ * Only works on UDP packets. Implements STAMP protocol.
  * 
- * TODO : we could improve the printing by buffering different print
- * and the making a single print
+ * TODO: finish protocol implementation. Receive timestamp done.
  * 
- * TODO : print TCP ports
+ * TODO: check UDP dest port.
+ * 
+ * Need to populate map with the "stamp_maps.py" script to work.
+ * If map is empty, the packet is dropped.
  */
 
 #define HIKE_DEBUG 1
@@ -23,21 +19,11 @@
 #define REAL
 //#define REPL
 
-#include <stddef.h>
-#include <linux/in.h>
-#include <linux/if_ether.h>
-#include <linux/if_packet.h>
-#include <linux/ipv6.h>
 #include <linux/udp.h>
-#include <linux/tcp.h>
-#include <linux/seg6.h>
-#include <linux/errno.h>
 
 #ifdef REAL
-  //#include "tb_defs.h"
   #include "hike_vm.h"
   #include "parse_helpers.h"
-  #include "ip6_hset.h"
 #endif  
 
 #ifdef REPL
@@ -48,41 +34,10 @@
 
 #endif
 
-#define STRING_FOO 1
-//#define NET_LAYER 2
-//#define TRANSP_LAYER 4
-
-/* show_pkt_info ()
- * 
- * 
- * 
- * input:
- * - ARG1:	HIKe Program ID;
- * - ARG2:  which parts of the packet needs to be printed
- * - ARG3:  user supplied info
- *
- * 
-*/
 bpf_map(map_time, HASH, __u8, __u64, 1);
 
 HIKE_PROG(HIKE_PROG_NAME) {
-#define BUF_LEN 10
-  struct pkt_info *info;
-  struct hdr_cursor *cur;
-  struct __shm_buff {
-    char p1[BUF_LEN];
-    char p2[BUF_LEN];
-  } *pshm;
-
   struct stamp {
-    __u32 seq_number;
-    __u64 timestamp;
-    __u16 error_estimate;
-    __u16 ssid;
-  } __attribute__((packed));
-  struct stamp *stamp_ptr;
-
-  struct stamp_refl {
     __u32 seq_number;
     __u64 timestamp;
     __u16 error_estimate;
@@ -94,39 +49,20 @@ HIKE_PROG(HIKE_PROG_NAME) {
     __u16 mbz_16;
     __u8 sess_send_ttl;
   } __attribute__((packed));
-  struct stamp_refl* stamp_refl_ptr;
 
-  union { 
-    struct ethhdr *eth_h;
-    struct ipv6hdr *ip6h;
-    struct udphdr *udph;
-    struct tcphdr *tcph;
-#define eth_h hdr.eth_h
-#define ip6h  hdr.ip6h
-#define udph  hdr.udph
-#define tcph  hdr.tcph
-  } hdr;
-
-  int select_match_string = HVM_ARG2;
-  int replace_char = HVM_ARG3;
-    
-  
-  int offset = 0;
-  int ret;
-
+  struct stamp* stamp_ptr;
+  __u64 receive_timestamp;
+  struct hdr_cursor *cur;
+  struct pkt_info *info;
+  struct udphdr *udph;  
   __u64 timestamp;
-  __u64 display2;
   __u16 udp_plen;
   __u16 udp_poff; //udp payload offset
-  __u64 new_timestamp;
   __u64 boottime;
-  __u64 *value;
+  int offset = 0;
+  __u64 *delta;
   __u8 key = 0;
-
-
-  char *p;
-  char *keyword;
-  int i;
+  int ret;
 
   /* retrieve packet information from HIKe shared memory*/
   info = hike_pcpu_shmem();
@@ -139,9 +75,10 @@ HIKE_PROG(HIKE_PROG_NAME) {
   cur = pkt_info_cur(info);
   /* no need for checking cur != NULL here */
 
-  // ipv6_find_hdr is defined in parse_helpers.h
-  // when the fourth parameter is -1, it returns the 
-  // "layer 4" final protocol
+  /* ipv6_find_hdr is defined in parse_helpers.h
+   * when the fourth parameter is -1, it returns the 
+   * "layer 4" final protocol
+   */
   ret = ipv6_find_hdr(ctx, cur, &offset, -1, NULL, NULL);
   if (unlikely(ret < 0)) {
     switch (ret) {
@@ -177,15 +114,14 @@ HIKE_PROG(HIKE_PROG_NAME) {
   //udp_len including header
   udp_plen = bpf_ntohs(udph->len) - sizeof(*udph);
   DEBUG_HKPRG_PRINT("UDP payload len: %d", udp_plen);
-  DEBUG_HKPRG_PRINT("sizeof stamp: %d", sizeof(struct stamp));
 
   udp_poff = offset + sizeof(*udph);
 
   HVM_RET = 0;
 
-  stamp_refl_ptr = (struct stamp_refl *)cur_header_pointer(ctx, cur, udp_poff, 
-                                    sizeof(*stamp_refl_ptr));
-  if (unlikely(!stamp_refl_ptr))
+  stamp_ptr = (struct stamp *)cur_header_pointer(ctx, cur, udp_poff, 
+                                    sizeof(*stamp_ptr));
+  if (unlikely(!stamp_ptr))
     goto drop;
 
 /* read boot time from kernel, read delta between kernel boot time
@@ -196,7 +132,7 @@ HIKE_PROG(HIKE_PROG_NAME) {
  * map with delta value, if map is empty, the packet is dropped.
  */
 
-  timestamp = *((__u64 *)&stamp_refl_ptr->timestamp) ;
+  timestamp = *((__u64 *)&stamp_ptr->timestamp) ;
   timestamp = bpf_be64_to_cpu(timestamp);
   DEBUG_HKPRG_PRINT("timestamp : %llx", timestamp); 
 
@@ -204,20 +140,15 @@ HIKE_PROG(HIKE_PROG_NAME) {
   if (unlikely(!boottime))
 		goto drop;
   DEBUG_HKPRG_PRINT("boot time: %llx", boottime);
-  value = bpf_map_lookup_elem(&map_time, &key);
-  if (unlikely(!value))
+  delta = bpf_map_lookup_elem(&map_time, &key);
+  if (unlikely(!delta))
 		goto drop;
-  DEBUG_HKPRG_PRINT("delta: %llx", *value);
-  new_timestamp = boottime + *value;
-  DEBUG_HKPRG_PRINT("new timestamp: %llx", new_timestamp);
-  new_timestamp = bpf_cpu_to_be64(new_timestamp);
-  DEBUG_HKPRG_PRINT("new timestamp be: %llx", new_timestamp);
-  stamp_refl_ptr->receive_timestamp = new_timestamp;
-
-
-
-match:
-  HVM_RET = 1;
+  DEBUG_HKPRG_PRINT("delta: %llx", *delta);
+  receive_timestamp = boottime + *delta;
+  DEBUG_HKPRG_PRINT("new timestamp: %llx", receive_timestamp);
+  receive_timestamp = bpf_cpu_to_be64(receive_timestamp);
+  DEBUG_HKPRG_PRINT("new timestamp be: %llx", receive_timestamp);
+  stamp_ptr->receive_timestamp = receive_timestamp;
 
 out:
 	return HIKE_XDP_VM;
@@ -231,14 +162,7 @@ drop:
 #undef udph  
 #undef tcph  
 }
-//EXPORT_HIKE_PROG(HIKE_PROG_NAME);
-EXPORT_HIKE_PROG_3(HIKE_PROG_NAME, __u64, select_match_string, __u64, replace_char);
-
-/* Export const */
-EXPORT_HIKE_CONST(STRING_FOO);
-//EXPORT_HIKE_CONST(NET_LAYER);
-//EXPORT_HIKE_CONST(TRANSP_LAYER);
-
+EXPORT_HIKE_PROG(HIKE_PROG_NAME);
 
 #ifdef REAL
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
